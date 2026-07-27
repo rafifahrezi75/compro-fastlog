21b1d11f66b3d3681f243656138be1ed83d9b4c4
farizzz29 <farizizza290405@gmail.com>
2026-07-27 15:36:07 +0700

    frontenduser

--- routes/web.php
+++ routes/web.php
@@ -2,82 +2,116 @@
 
 use Illuminate\Support\Facades\Route;
 
+// // dashboard pages
+// Route::get('/', function () {
+//     return view('pages.dashboard.ecommerce', ['title' => 'E-commerce Dashboard']);
+// })->name('dashboard');
+
+// // calender pages
+// Route::get('/calendar', function () {
+//     return view('pages.calender', ['title' => 'Calendar']);
+// })->name('calendar');
+
+// // profile pages
+// Route::get('/profile', function () {
+//     return view('pages.profile', ['title' => 'Profile']);
+// })->name('profile');
+
+// // form pages
+// Route::get('/form-elements', function () {
+//     return view('pages.form.form-elements', ['title' => 'Form Elements']);
+// })->name('form-elements');
+
+// // tables pages
+// Route::get('/basic-tables', function () {
+//     return view('pages.tables.basic-tables', ['title' => 'Basic Tables']);
+// })->name('basic-tables');
+
+// // pages
+
+// Route::get('/blank', function () {
+//     return view('pages.blank', ['title' => 'Blank']);
+// })->name('blank');
+
+// // error pages
+// Route::get('/error-404', function () {
+//     return view('pages.errors.error-404', ['title' => 'Error 404']);
+// })->name('error-404');
+
+// // chart pages
+// Route::get('/line-chart', function () {
+//     return view('pages.chart.line-chart', ['title' => 'Line Chart']);
+// })->name('line-chart');
+
+// Route::get('/bar-chart', function () {
+//     return view('pages.chart.bar-chart', ['title' => 'Bar Chart']);
+// })->name('bar-chart');
+
+
+// // authentication pages
+// Route::get('/signin', function () {
+//     return view('pages.auth.signin', ['title' => 'Sign In']);
+// })->name('signin');
+
+// Route::get('/signup', function () {
+//     return view('pages.auth.signup', ['title' => 'Sign Up']);
+// })->name('signup');
+
+// // ui elements pages
+// Route::get('/alerts', function () {
+//     return view('pages.ui-elements.alerts', ['title' => 'Alerts']);
+// })->name('alerts');
+
+// Route::get('/avatars', function () {
+//     return view('pages.ui-elements.avatars', ['title' => 'Avatars']);
+// })->name('avatars');
+
+// Route::get('/badge', function () {
+//     return view('pages.ui-elements.badges', ['title' => 'Badges']);
+// })->name('badges');
+
+// Route::get('/buttons', function () {
+//     return view('pages.ui-elements.buttons', ['title' => 'Buttons']);
+// })->name('buttons');
+
+// Route::get('/image', function () {
+//     return view('pages.ui-elements.images', ['title' => 'Images']);
+// })->name('images');
+
+// Route::get('/videos', function () {
+//     return view('pages.ui-elements.videos', ['title' => 'Videos']);
+// })->name('videos');
+
+// WEB USER
 // dashboard pages
 Route::get('/', function () {
-    return view('pages.dashboard.ecommerce', ['title' => 'E-commerce Dashboard']);
-})->name('dashboard');
+    return view('user.pages.index');
+})->name('home');
 
-// calender pages
-Route::get('/calendar', function () {
-    return view('pages.calender', ['title' => 'Calendar']);
-})->name('calendar');
+Route::get('/about', function () {
+    return view('user.pages.about');
+})->name('about');
 
-// profile pages
-Route::get('/profile', function () {
-    return view('pages.profile', ['title' => 'Profile']);
-})->name('profile');
+Route::get('/services', function () {
+    return view('user.pages.services');
+})->name('services');
 
-// form pages
-Route::get('/form-elements', function () {
-    return view('pages.form.form-elements', ['title' => 'Form Elements']);
-})->name('form-elements');
+Route::get('/blog', function () {
+    return view('user.pages.blog');
+})->name('blog');
 
-// tables pages
-Route::get('/basic-tables', function () {
-    return view('pages.tables.basic-tables', ['title' => 'Basic Tables']);
-})->name('basic-tables');
+Route::get('/blog-details', function () {
+    return view('user.pages.blog-details');
+})->name('blog-details');
 
-// pages
+Route::get('/contact', function () {
+    return view('user.pages.contact');
+})->name('contact');
 
-Route::get('/blank', function () {
-    return view('pages.blank', ['title' => 'Blank']);
-})->name('blank');
+Route::get('/gallery', function () {
+    return view('user.pages.gallery');
+})->name('gallery');
 
-// error pages
-Route::get('/error-404', function () {
-    return view('pages.errors.error-404', ['title' => 'Error 404']);
-})->name('error-404');
-
-// chart pages
-Route::get('/line-chart', function () {
-    return view('pages.chart.line-chart', ['title' => 'Line Chart']);
-})->name('line-chart');
-
-Route::get('/bar-chart', function () {
-    return view('pages.chart.bar-chart', ['title' => 'Bar Chart']);
-})->name('bar-chart');
-
-
-// authentication pages
-Route::get('/signin', function () {
-    return view('pages.auth.signin', ['title' => 'Sign In']);
-})->name('signin');
-
-Route::get('/signup', function () {
-    return view('pages.auth.signup', ['title' => 'Sign Up']);
-})->name('signup');
-
-// ui elements pages
-Route::get('/alerts', function () {
-    return view('pages.ui-elements.alerts', ['title' => 'Alerts']);
-})->name('alerts');
-
-Route::get('/avatars', function () {
-    return view('pages.ui-elements.avatars', ['title' => 'Avatars']);
-})->name('avatars');
-
-Route::get('/badge', function () {
-    return view('pages.ui-elements.badges', ['title' => 'Badges']);
-})->name('badges');
-
-Route::get('/buttons', function () {
-    return view('pages.ui-elements.buttons', ['title' => 'Buttons']);
-})->name('buttons');
-
-Route::get('/image', function () {
-    return view('pages.ui-elements.images', ['title' => 'Images']);
-})->name('images');
-
-Route::get('/videos', function () {
-    return view('pages.ui-elements.videos', ['title' => 'Videos']);
-})->name('videos');
+Route::get('/career', function () {
+    return view('user.pages.career');
+})->name('career');
